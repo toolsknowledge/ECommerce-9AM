c98d6082654757bde01e773b2178a603e222a903
Samba <samba.pendela@gmail.com>
2021-06-03 09:58:33 +0530

    code

--- ecommerce/src/screens/ProductScreen.tsx
+++ ecommerce/src/screens/ProductScreen.tsx
@@ -2,9 +2,14 @@ import React, { Component } from "react";
 import { match as Match, NavLink } from "react-router-dom";
 import { connect } from "react-redux";
 import getDetails from "../actions/DetailActions";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
+import Rating from "../components/Rating";
 
 
-interface IState{}
+interface IState{
+    qty : number;
+}
 
 interface IProps{
     match : Match<paramRoutes>;
@@ -20,7 +25,17 @@ interface paramRoutes{
 class ProductScreen extends Component<IProps,IState>{
     constructor(props:IProps){
         super(props);
+        this.state = {
+            qty : 1
+        }
     }
+
+
+    setQty = (num:any)=>{
+        this.setState({
+            qty : num
+        })
+    };
 
     componentDidMount(){
         this.props.getDetilsById(this.props.match.params.id);
@@ -30,8 +45,83 @@ class ProductScreen extends Component<IProps,IState>{
         const { loading,product,error } = this.props.res;
         return(
             <React.Fragment>
-                <NavLink to="/" className="back_screen">back to home</NavLink>
-                <h1>{JSON.stringify(loading)}....{JSON.stringify(product)}.....{error}</h1>
+                {
+                    !loading?(<LoadingBox></LoadingBox>):error === "Network Error"?(<MessageBox variant="danger">{error}</MessageBox>):(<div>
+                        <NavLink to="/" className="back_screen">back to home</NavLink>
+                        <div className="row top">
+                            <div className="col-2">
+                                <img src={product.image} alt={product.name}></img>
+                            </div>
+
+                            <div className="col-1">
+                                <ul>
+                                    <li><h2>{product.name}</h2></li>
+                                    <li>
+                                        <Rating rating={product.rating} numReviews={product.numReviews}></Rating>
+                                    </li>
+                                    <li>
+                                        $ {product.price}
+                                    </li>
+                                    <li>
+                                        {product.description}
+                                    </li>
+                                </ul>
+                            </div>
+
+                            <div className="col-1">
+                                <div className="card card-body">
+                                    <ul>
+                                        <li>
+                                            <div className="row">
+                                                <div>Price</div>
+                                                <div>$ {product.price}</div>
+                                            </div>
+                                        </li>
+
+                                        <li>
+                                            <div className="row">
+                                                <div>Avilability</div>
+                                                <div>
+                                                    {product.countInStock>0?(<span className="success">Stock Available</span>):(<span className="error">Out of Stock</span>)}
+                                                </div>
+                                            </div>
+                                        </li>
+
+                                        {product.countInStock > 0 && (
+                                            <>
+                                                <li>
+                                                    <div className="row">
+                                                        <div>Qty</div>
+                                                        {/** dropdown */}
+                                                        <div>
+                                                            <select value={this.state.qty}
+                                                                    onChange={(e:any)=>this.setQty(e.target.value)}>
+                                                                
+                                                                {
+                                                                    [...Array(product.countInStock).keys()].map((x:any)=>(
+                                                                        <option key={x+1} value={x+1}>{x+1}</option>
+                                                                    ))
+                                                                }
+                                                            </select>
+                                                        </div>
+                                                    
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <button className="primary block">Add to Cart</button>
+
+                                                </li>
+                                            </>
+                                        )}
+
+
+                                    </ul>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>)
+                }
             </React.Fragment>
         )
     }
