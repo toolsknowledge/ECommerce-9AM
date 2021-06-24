9c1b1e9b93569e538aba510e4f34b36639d08b12
Samba <samba.pendela@gmail.com>
2021-06-24 09:39:39 +0530

    Signin Module in Progress

--- ecommerce/src/screens/SigninScreen.tsx
+++ ecommerce/src/screens/SigninScreen.tsx
@@ -9,20 +9,49 @@ interface IProps{
 }
 
 class SigninScreen extends Component<IProps,IState>{
+    
+    u_email = React.createRef<HTMLInputElement>();
+    u_pwd = React.createRef<HTMLInputElement>();
+
+
     constructor(props:IProps){
         super(props);
     }
 
-    login = ()=>{
-        this.props.login_Fn({"email":"hr@ashokit.in","password":"admin"});
+    login = (e:any)=>{
+        e.preventDefault();
+        this.props.login_Fn({"email":this.u_email.current?.value,"password":this.u_pwd.current?.value});
     };
 
     render(){
         return(
             <React.Fragment>
-                <button onClick={this.login}>Signin</button>
-                <br></br><br></br>
-                {this.props.res.user_details.image}
+                <div className="row1">
+                    <form  className="form" onSubmit={this.login}>
+                        <div>
+                            <h1>Sign In</h1>
+                        </div>
+                        <div>
+                            <label>Email Address</label>
+                            <input type="email" name="email" ref={this.u_email} required />
+                        </div>
+                        <div>
+                            <label>Password</label>
+                            <input type="password" name="password" ref={this.u_pwd} required />
+                        </div>
+                        <div> 
+                            <label><input type="checkbox" value="isAdmin"/> is Admin</label>
+                        </div>
+                        <div>
+                            <label></label>
+                            <button type="submit" className="btn-warning">Sign In</button>
+                        </div>
+                        <div>
+                            <label></label>
+                            <div>New Customer<a style={{color: "blue"}}>Create New Account</a></div>
+                        </div>
+                    </form>
+                </div>
             </React.Fragment>
         )
     }
